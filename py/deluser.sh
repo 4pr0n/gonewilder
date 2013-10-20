48e4cb84cbe0bb0000c86c61571c2d35e3dd1df8
4pr0n <1fakeyfake@gmail.com>
2013-10-20 13:50:56 -0700

    script for deleting users

---
+++ py/deluser.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Quickly delete all traces of a user from the database and filesystem
+# usage: ./deluser.sh username
+
+USER=$1
+USERID=`sqlite3 ../database.db "select id from users where username = '${USER}'"`
+for table in albums comments images posts; do
+	sqlite3 ../database.db "delete from ${table} where userid = ${USERID}"
+done
+sqlite3 ../database.db "delete from users where id = ${USERID}"
+rm -r ../content/${USER}/
