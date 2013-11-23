22fdeee23edcbad9a2f18199f6addaf3a6aa74ec
4pr0n <1fakeyfake@gmail.com>
2013-11-23 07:21:06 -0800

    minor changes, reformatting

--- py/populatedb.py
+++ py/populatedb.py
@@ -26,11 +26,11 @@ def populate_db():
 			itempath = path.join(userdir, item)
 			if path.isfile(itempath):
 				# Image
-				print "image: %s" % itempath
+				#print "image: %s" % itempath
 				db.add_existing_image(user, item, itempath)
 			elif path.isdir(itempath):
 				# Album
-				print "album: %s" % itempath
+				#print "album: %s" % itempath
 				db.add_existing_album(user, item, itempath)
 
 '''
@@ -40,12 +40,22 @@ def populate_db():
 '''
 def set_last_since():
 	cur = db.conn.cursor()
-	query = 'select username,themax from users,(select userid,max(posts.id) as themax from posts group by posts.userid) where userid = users.id'
+	query = '''
+		select username,themax 
+		from 
+			users,
+			(
+				select userid,max(posts.id) as themax 
+				from posts 
+				group by posts.userid
+			)
+		where userid = users.id
+	'''
 	for user,since in cur.execute(query).fetchall():
 		print user,since
 		db.set_last_since_id(user, since)
 
 if __name__ == '__main__':
-	#populate_db()
+	populate_db()
 	#set_last_since()
 	pass
